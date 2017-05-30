36134de28b48fb0b8f21288efb4f88bb679bc327
Arlo Shefte-Jacobs <arlo.sj3@gmail.com>
2017-05-30 13:59:07 -0600

    cookies

--- server.js
+++ server.js
@@ -1,16 +1,21 @@
 const express = require('express');
 const app = express();
+var cookieParser = require('cookie-parser');
+app.use(cookieParser());
 
 app.get('/set', (req,res,next) => {
-  
+res.cookie('foo','bar');
+res.send();
 });
 
 app.get('/read', (req,res,next) => {
-
+req.cookies;
+res.send(req.cookies);
 });
 
 app.get('/clear', (req,res,next) => {
-
+res.clearCookie('deleteMe');
+res.send();
 });
 
 app.listen('3000', () => {
